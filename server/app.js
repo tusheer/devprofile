beb97bf0e7e9db9a97c80969351e5f5fb7a00b3d
jaantusher <jaanealamtusher@gmail.com>
2019-06-18 11:15:39 +0600

    something was missing

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ const post = require('./routes/post.js');
 const profile = require('./routes/profile.js');
 const users = require('./routes/users.js');
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,headers');
+	next();
+});
+
 app.use(bodyParser.json());
 app.use('/post', post);
 app.use('/profile', profile);
