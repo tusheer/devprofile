0b14bbe3a9df46839d387ebcbeb038e5b5480def
jaantusher <jaanealamtusher@gmail.com>
2019-07-13 08:32:05 +0600

    some essue fixed

--- client/tusher/src/Component/devprofile.js
+++ client/tusher/src/Component/devprofile.js
@@ -9,32 +9,36 @@ export class devprofile extends Component {
 					<p>Connect with developars</p>
 				</div>
 				<div className="profile container">
-					<div className=" col-lg-6">
-						<div className="single_profile">
+					<div className="col-md-6">
+						<div className="single_profile ">
 							<div className="image">
-								<img
-									src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-									alt=""
-								/>
+								<div className="image_wraper">
+									<img
+										src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+										alt=""
+									/>
+								</div>
 							</div>
 							<div className="profile_details">
-								<div className="name">
-									<h3>Jane Alam Tusehr</h3>
-									<p>Full Stack Web developar</p>
-								</div>
-								<div className="skill">
-									<div>
-										<span>Html</span>
-										<span>Css</span>
-										<span>React js</span>
-										<span>JavaScript</span>
-										<span>Node js</span>
-										<span>Angular</span>
-										<span>Redux</span>
-										<span>MongoDB</span>
+								<div>
+									<div className="name">
+										<h3>Jane Alam Tusehr</h3>
+										<p>Full Stack Web developar</p>
 									</div>
-									<div>
-										<button className="btn">View Profile</button>
+									<div className="skill">
+										<div>
+											<span>Html</span>
+											<span>Css</span>
+											<span>React js</span>
+											<span>JavaScript</span>
+											<span>Node js</span>
+											<span>Angular</span>
+											<span>Redux</span>
+											<span>MongoDB</span>
+										</div>
+										<div>
+											<button className="btn">View Profile</button>
+										</div>
 									</div>
 								</div>
 							</div>
--- client/tusher/src/Component/personal.js
+++ client/tusher/src/Component/personal.js
@@ -45,7 +45,7 @@ function Body() {
 		<div className="body container-fluid">
 			<div className="container">
 				<div className="row">
-					<div className="col-md-4 left">
+					<div className="col-md-5 col-lg-4 left">
 						<div className="bio">
 							<h3>
 								<i class="fa fa-info-circle" />Bio
@@ -58,23 +58,80 @@ function Body() {
 						<div className="skill">
 							<div>
 								<h3>
-									<i class="fa fa-pencil-ruler" />Skill Set
+									<i class="fa fa-pencil" />Skill Set
 								</h3>
 							</div>
-							<span>Html</span>
-							<span>Css</span>
-							<span>React js</span>
-							<span>JavaScript</span>
-							<span>Node js</span>
-							<span>Angular</span>
-							<span>Redux</span>
-							<span>MongoDB</span>
+							<div className="span_wraper">
+								<span>Html</span>
+								<span>Css</span>
+								<span>React js</span>
+								<span>JavaScript</span>
+								<span>Node js</span>
+								<span>Angular</span>
+								<span>Redux</span>
+								<span>MongoDB</span>
+							</div>
+						</div>
+						<div className="education">
+							<h3>
+								<i class="fa fa-graduation-cap" />Education
+							</h3>
+							<div className="education_wraper">
+								<div className="single_education">
+									<h4>University of Chittagong</h4>
+									<p>
+										<span>2016/02</span> - <span>2019/01</span>
+									</p>
+									<p>
+										<b>location: </b>
+										<span>Chittagong, Bangladesh</span>
+									</p>
+									<p>
+										<b>Discription:</b> <span>Full Stack Web Developar</span>
+									</p>
+								</div>
+							</div>
 						</div>
 					</div>
 
-					<div className="col-md-8 right">
-						<div className="experience">Experience</div>
-						<div className="education">Education</div>
+					<div className="col-md-7 col-lg-8 right">
+						<div className="experience">
+							<h3>
+								<i class="fa fa-briefcase" /> Experience
+							</h3>
+							<div className="experience_wraper">
+								<div className="single_experience">
+									<h4>TechTop Software Ltd.</h4>
+									<p>
+										<span>2016/02</span> - <span>Now</span>
+									</p>
+									<p>
+										<b>location: </b> <span>Dhaka, Bangladesh</span>
+									</p>
+									<p>
+										<b>Discription:</b> <span>Full Stack Web Developar</span>
+									</p>
+								</div>
+								<div className="single_experience">
+									<h4>TechTop Software Ltd.</h4>
+									<p>
+										<span>2016/02</span> - <span>Now</span>
+									</p>
+									<p>
+										<b>Position: </b>
+										<span>Senior Developar</span>
+									</p>
+									<p>
+										<b>location: </b>
+										<span>Dhaka, Bangladesh</span>
+									</p>
+									<p>
+										<b>Discription: </b>
+										<span>Full Stack Web Developar</span>
+									</p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
