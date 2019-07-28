86cb8f65d45d2bfb2a6f209b0ffd1903245fa8f0
jaantusher <jaanealamtusher@gmail.com>
2019-07-28 16:37:14 +0600

    handle sign up controller

--- client/src/Component/signup.js
+++ client/src/Component/signup.js
@@ -1,60 +1,89 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
+import authContext from '../contex/auth/authContext';
 
-export default class Signup extends Component {
-	render() {
-		return (
-			<div className="signup padding_top">
-				<div className="signup_header pt-4">
-					<h1 className="text-center">Sign up</h1>
-					<p className="text-center">Create your DevConnect account.</p>
-				</div>
-				<div className="signup_form container">
-					<div className="signup_form_wraper">
-						<form>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Name</label>
-								<input type="text" class="form-control" placeholder="Enter your name" />
-							</div>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Email address</label>
-								<input
-									type="email"
-									class="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-									placeholder="Enter email"
-								/>
-								<small id="emailHelp" class="form-text text-muted">
-									We'll never share your email with anyone else.
-								</small>
-							</div>
-							<div class="form-group">
-								<label for="exampleInputPassword1">Password</label>
-								<input
-									type="password"
-									class="form-control"
-									id="exampleInputPassword1"
-									placeholder="Password"
-								/>
-							</div>
+const Signup = (props) => {
+	const context = useContext(authContext);
+	const { register } = context;
+	const [ form, setFrom ] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	});
+	const onChange = (e) => setFrom({ ...form, [e.target.name]: e.target.value });
+	const onSubmit = (e) => {
+		e.preventDefault();
 
-							<div class="form-group">
-								<label for="exampleInputPassword1">Confirm Password</label>
-								<input
-									type="password"
-									class="form-control"
-									id="exampleInputPassword1"
-									placeholder="Confirm Password"
-								/>
-							</div>
+		register(form);
+	};
+	const { name, email, password, password2 } = form;
 
-							<button type="submit" class="btn btn-primary">
-								Submit
-							</button>
-						</form>
-					</div>
+	return (
+		<div className="signup padding_top">
+			<div className="signup_header pt-4">
+				<h1 className="text-center">Sign up</h1>
+				<p className="text-center">Create your DevConnect account.</p>
+			</div>
+			<div className="signup_form container">
+				<div className="signup_form_wraper">
+					<form onSubmit={onSubmit}>
+						<div className="form-group">
+							<label htmlFor="exampleInputEmail1">Name</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Enter your name"
+								name="name"
+								value={name}
+								onChange={onChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="exampleInputEmail1">Email address</label>
+							<input
+								value={email}
+								type="email"
+								className="form-control"
+								aria-describedby="emailHelp"
+								placeholder="Enter email"
+								name="email"
+								onChange={onChange}
+							/>
+							<small id="emailHelp" className="form-text text-muted">
+								We'll never share your email with anyone else.
+							</small>
+						</div>
+						<div className="form-group">
+							<label htmlFor="exampleInputPassword1">Password</label>
+							<input
+								value={password}
+								type="password"
+								className="form-control"
+								placeholder="Password"
+								name="password"
+								onChange={onChange}
+							/>
+						</div>
+
+						<div className="form-group">
+							<label htmlFor="exampleInputPassword1">Confirm Password</label>
+							<input
+								value={password2}
+								type="password"
+								className="form-control"
+								placeholder="Confirm Password"
+								name="password2"
+								onChange={onChange}
+							/>
+						</div>
+
+						<button type="submit" className="btn btn-primary">
+							Submit
+						</button>
+					</form>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+export default Signup;
