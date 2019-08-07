8340f4c3e9daf407c3188a779f6b8815073feec2
jaantusher <jaanealamtusher@gmail.com>
2019-08-07 13:00:27 +0600

    ready to deploy

--- app.js
+++ app.js
@@ -48,15 +48,19 @@ app.use(
 		fileFilter,
 	}).single('avatar'),
 );
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 app.use('/api/users', users);
 app.use('/post', post);
 app.use('/profile', profile);
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 4000;
 mongoose
 	.connect('mongodb://localhost:27017/users', { useNewUrlParser: true })
 	.then(app.listen(port, () => console.log('game on')));
