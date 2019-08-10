4baaaec23d285339b298628e8534e850459ccb11
jaantusher <jaanealamtusher@gmail.com>
2019-08-10 09:00:41 +0600

    Fixed

--- app.js
+++ app.js
@@ -48,12 +48,12 @@ app.use(
 		fileFilter,
 	}).single('avatar'),
 );
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+// Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
 app.use('/api/users', users);
 app.use('/post', post);
 app.use('/profile', profile);
